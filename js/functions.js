4abc789fd393dadd70d3a796dd3d28b55218c590
window-ops <vladbruma2020@gmail.com>
2023-11-20 21:39:11 +0000

    Update 6 files
    - /css/custom.css
    - /css/7.css
    - /images/background.jpg
    - /js/functions.js
    - /index.html
    - /README.md

---
+++ js/functions.js
@@ -0,0 +1,141 @@
+let hasResult = false;
+
+function appendToDisplay(value) {
+  if (document.getElementById('display').value === 'Error' || hasResult) {
+    return;
+  }
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+  enableButtons();
+  hasResult = false;
+}
+
+function calculate() {
+  try {
+    document.getElementById('display').value = eval(document.getElementById('display').value);
+    disableButtons();
+    hasResult = true;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+    disableButtons();
+    hasResult = true;
+  }
+}
+
+function disableButtons() {
+  document.getElementById('button-0').disabled = true;
+  document.getElementById('button-1').disabled = true;
+  document.getElementById('button-2').disabled = true;
+  document.getElementById('button-3').disabled = true;
+  document.getElementById('button-4').disabled = true;
+  document.getElementById('button-5').disabled = true;
+  document.getElementById('button-6').disabled = true;
+  document.getElementById('button-7').disabled = true;
+  document.getElementById('button-8').disabled = true;
+  document.getElementById('button-9').disabled = true;
+  document.getElementById('button-add').disabled = true;
+  document.getElementById('button-subtract').disabled = true;
+  document.getElementById('button-multiply').disabled = true;
+  document.getElementById('button-divide').disabled = true;
+  document.getElementById('button-decimal').disabled = true;
+}
+
+function enableButtons() {
+  document.getElementById('button-0').disabled = false;
+  document.getElementById('button-1').disabled = false;
+  document.getElementById('button-2').disabled = false;
+  document.getElementById('button-3').disabled = false;
+  document.getElementById('button-4').disabled = false;
+  document.getElementById('button-5').disabled = false;
+  document.getElementById('button-6').disabled = false;
+  document.getElementById('button-7').disabled = false;
+  document.getElementById('button-8').disabled = false;
+  document.getElementById('button-9').disabled = false;
+  document.getElementById('button-add').disabled = false;
+  document.getElementById('button-subtract').disabled = false;
+  document.getElementById('button-multiply').disabled = false;
+  document.getElementById('button-divide').disabled = false;
+  document.getElementById('button-decimal').disabled = false;
+}
+
+function minimizeWindow() {
+  document.getElementById('window').style.display = "none";
+  document.getElementById('windowActions').style.display = "block";
+}
+
+function closeWindow() {
+  document.getElementById('window').remove();
+  document.getElementById('windowActions').remove();
+}
+
+function restoreWindow() {
+  document.getElementById('window').style.display = "block";
+  document.getElementById('windowActions').style.display = "none";
+}
+
+// Drag Window
+
+let isDragging = false;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('title-bar').addEventListener('mousedown', dragStart);
+  document.getElementById('title-bar').addEventListener('mouseup', dragEnd);
+  document.getElementById('title-bar').addEventListener('mousemove', drag);
+  document.getElementById('title-bar').addEventListener('touchstart', dragStart);
+  document.getElementById('title-bar').addEventListener('touchend', dragEnd);
+  document.getElementById('title-bar').addEventListener('touchmove', drag);
+});
+
+function dragStart(e) {
+  if (e.type === 'touchstart') {
+    initialX = e.touches[0].clientX - xOffset;
+    initialY = e.touches[0].clientY - yOffset;
+  } else {
+    initialX = e.clientX - xOffset;
+    initialY = e.clientY - yOffset;
+  }
+
+  if (e.target === document.getElementById('title-bar')) {
+    isDragging = true;
+    // Store the initial position of the window
+    initialWindowX = parseFloat(getComputedStyle(document.getElementById('window')).transform.split(',')[4]);
+    initialWindowY = parseFloat(getComputedStyle(document.getElementById('window')).transform.split(',')[5]);
+  }
+}
+
+function drag(e) {
+  if (isDragging) {
+    e.preventDefault();
+
+    if (e.type === 'touchmove') {
+      xOffset = e.touches[0].clientX - initialX;
+      yOffset = e.touches[0].clientY - initialY;
+    } else {
+      xOffset = e.clientX - initialX;
+      yOffset = e.clientY - initialY;
+    }
+
+    // Calculate the new position of the window relative to the initial position
+    let newX = initialWindowX + xOffset;
+    let newY = initialWindowY + yOffset;
+
+    setTranslate(newX, newY, document.getElementById('window'));
+  }
+}
+
+function dragEnd() {
+  xOffset = 0; 
+  yOffset = 0;
+  isDragging = false;
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+}
