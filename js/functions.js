ca7b0f909dac204a24d6635992987e7df6933c42
window-ops <vladbruma2020@gmail.com>
2023-11-21 17:46:05 +0000

    Update file functions.js

--- js/functions.js
+++ js/functions.js
@@ -181,7 +181,9 @@ function openAboutWindow() {
     '<button aria-label=\"Close\" onclick=\"closeAboutWindow()\"></button>' +
     '</div>' +
     '</div>' +
-    '<div class=\"window-body has-space\">A Windows 7-styled calculator designed for users who prefer the classic look over the UWP calculator in Windows 11. This fast-loading calculator is good for quick calculations, but please note that it does not currently support scientific mode.</div>';
+    '<div class=\"window-body has-space\">' +
+    '<p>A Windows 7-styled calculator designed for users who prefer the classic look over the UWP calculator in Windows 11. This fast-loading calculator is good for quick calculations, but please note that it does not currently support scientific mode.</p>' +
+    '</div>';
   document.body.appendChild(aboutWindow);
   enableGrab(aboutWindow);
   document.getElementById('aboutWindowActions').style.display = "none";
