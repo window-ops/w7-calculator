3057b40735599525f9ae4481831ff7e1ae69587a
window-ops <vladbruma2020@gmail.com>
2023-11-21 15:50:40 +0000

    Update file functions.js

--- js/functions.js
+++ js/functions.js
@@ -88,67 +88,76 @@ function restoreCalculatorWindow() {
   document.getElementById('calculatorWindowActions').style.display = "none";
 }
 
-// Drag Calculator Window
-
-let isDragging = false;
-let initialX;
-let initialY;
-let xOffset = 0;
-let yOffset = 0;
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('calculatorWindowTitleBar').addEventListener('mousedown', dragStart);
-  document.getElementById('calculatorWindowTitleBar').addEventListener('mouseup', dragEnd);
-  document.getElementById('calculatorWindowTitleBar').addEventListener('mousemove', drag);
-  document.getElementById('calculatorWindowTitleBar').addEventListener('touchstart', dragStart);
-  document.getElementById('calculatorWindowTitleBar').addEventListener('touchend', dragEnd);
-  document.getElementById('calculatorWindowTitleBar').addEventListener('touchmove', drag);
-});
-
-function dragStart(e) {
-  if (e.type === 'touchstart') {
-    initialX = e.touches[0].clientX - xOffset;
-    initialY = e.touches[0].clientY - yOffset;
-  } else {
-    initialX = e.clientX - xOffset;
-    initialY = e.clientY - yOffset;
-  }
-
-  if (e.target === document.getElementById('calculatorWindowTitleBar')) {
-    isDragging = true;
-    initialWindowX = parseFloat(getComputedStyle(document.getElementById('calculatorWindow')).transform.split(',')[4]);
-    initialWindowY = parseFloat(getComputedStyle(document.getElementById('calculatorWindow')).transform.split(',')[5]);
-  }
-}
-
-function drag(e) {
-  if (isDragging) {
-    e.preventDefault();
-
-    if (e.type === 'touchmove') {
-      xOffset = e.touches[0].clientX - initialX;
-      yOffset = e.touches[0].clientY - initialY;
-    } else {
-      xOffset = e.clientX - initialX;
-      yOffset = e.clientY - initialY;
-    }
-
-    let newX = initialWindowX + xOffset;
-    let newY = initialWindowY + yOffset;
-
-    setTranslate(newX, newY, document.getElementById('calculatorWindow'));
-  }
-}
-
-function dragEnd() {
-  xOffset = 0; 
-  yOffset = 0;
-  isDragging = false;
-}
+// Drag
 
 function setTranslate(xPos, yPos, el) {
   el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
 }
+
+function enableGrab(windowElement) {
+  let isDragging = false;
+  let initialX;
+  let initialY;
+  let xOffset = 0;
+  let yOffset = 0;
+
+  function dragStart(e) {
+    if (e.type === 'touchstart') {
+      initialX = e.touches[0].clientX - xOffset;
+      initialY = e.touches[0].clientY - yOffset;
+    } else {
+      initialX = e.clientX - xOffset;
+      initialY = e.clientY - yOffset;
+    }
+
+    if (e.target === windowElement.querySelector('.title-bar')) {
+      isDragging = true;
+      let transform = getComputedStyle(windowElement).transform;
+      let initialWindowX = parseFloat(transform.split(',')[4]);
+      let initialWindowY = parseFloat(transform.split(',')[5]);
+      windowElement.dataset.initialWindowX = initialWindowX;
+      windowElement.dataset.initialWindowY = initialWindowY;
+    }
+  }
+
+  function drag(e) {
+    if (isDragging) {
+      e.preventDefault();
+      if (e.type === 'touchmove') {
+        xOffset = e.touches[0].clientX - initialX;
+        yOffset = e.touches[0].clientY - initialY;
+      } else {
+        xOffset = e.clientX - initialX;
+        yOffset = e.clientY - initialY;
+      }
+
+      let newX = parseFloat(windowElement.dataset.initialWindowX) + xOffset;
+      let newY = parseFloat(windowElement.dataset.initialWindowY) + yOffset;
+
+      setTranslate(newX, newY, windowElement);
+    }
+  }
+
+  function dragEnd() {
+    xOffset = 0;
+    yOffset = 0;
+    isDragging = false;
+  }
+
+  windowElement.querySelector('.title-bar').addEventListener('mousedown', dragStart);
+  windowElement.querySelector('.title-bar').addEventListener('mouseup', dragEnd);
+  windowElement.querySelector('.title-bar').addEventListener('mousemove', drag);
+  windowElement.querySelector('.title-bar').addEventListener('touchstart', dragStart);
+  windowElement.querySelector('.title-bar').addEventListener('touchend', dragEnd);
+  windowElement.querySelector('.title-bar').addEventListener('touchmove', drag);
+}
+
+// Drag Calculator Window
+
+document.addEventListener('DOMContentLoaded', function() {
+  let calculatorWindow = document.getElementById('calculatorWindow');
+  enableGrab(calculatorWindow);
+});
 
 // About Window
 
@@ -161,6 +170,9 @@ function openAboutWindow() {
                         '</div>' +
                         '</div>' +
                         '<div class="window-body has-space"></div>' +
-                        '</div>'
-  document.body.appendChild(aboutWindowHTML);
+                        '</div>';
+  let aboutWindow = document.createElement('div');
+  aboutWindow.innerHTML = aboutWindowHTML;
+  document.body.appendChild(aboutWindow);
+  enableGrab(aboutWindow);
 }
