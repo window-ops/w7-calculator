91d830bc39a294f716c588b2374bb2906cf449f5
window-ops <vladbruma2020@gmail.com>
2023-11-29 09:40:47 +0000

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -194,6 +194,7 @@ function openAboutWindow() {
     '<p>A Windows 7-styled calculator designed for users who prefer the classic look over the UWP calculator in Windows 11. This fast-loading calculator is good for quick calculations, but please note that it does not currently support scientific mode.</p>' +
     '</div>';
   document.body.appendChild(aboutWindow);
+  bringWindowToFront(aboutWindow);
   enableGrab(aboutWindow);
   document.getElementById('aboutWindowActions').style.display = "none";
 }
